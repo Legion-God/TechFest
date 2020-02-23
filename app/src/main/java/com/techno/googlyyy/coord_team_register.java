9c157c02a86e0a642cefe023f8c592cb003ed6bc
Shubhendra Rajkarne <srajkarne9@gmail.com>
2020-02-24 02:20:40 +0530

    database connected and working

--- app/src/main/java/com/techno/googlyyy/coord_team_register.java
+++ app/src/main/java/com/techno/googlyyy/coord_team_register.java
@@ -3,12 +3,51 @@ package com.techno.googlyyy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class coord_team_register extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class coord_team_register extends AppCompatActivity implements View.OnClickListener {
+    //only for sumo class
+
+    private Button register;
+    private DatabaseReference mDatabase;
+    private EditText team_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coord_team_register);
+
+        register = findViewById(R.id.register_btn);
+        register.setOnClickListener(this);
+
+        team_text = findViewById(R.id.team_name);
+
+       mDatabase = FirebaseDatabase.getInstance().getReference("sumo");
+    }
+
+    private void addTeam(){
+        String team = team_text.getText().toString().trim();
+        if(!team.isEmpty()){
+
+          cmn_structure sumo = new cmn_structure(team);
+          mDatabase.child("teams").setValue(sumo);
+            Toast.makeText(this, "Team Added", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Enter the Team name", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if(id == R.id.register_btn){
+            addTeam();
+        }
     }
 }
--- app/src/main/java/com/techno/googlyyy/event_menu.java
+++ app/src/main/java/com/techno/googlyyy/event_menu.java
@@ -6,12 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class event_menu extends AppCompatActivity implements View.OnClickListener{
 
 
     private Button lan_game;
     private Button sumo;
+    private Button settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,15 @@ public class event_menu extends AppCompatActivity implements View.OnClickListene
 
         sumo = findViewById(R.id.sumo_btn);
         sumo.setOnClickListener(this);
+
+        settings = findViewById(R.id.settings_btn);
+        settings.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "Go to settings to Logout", Toast.LENGTH_SHORT).show();
     }
 
     public void goToLan(){
@@ -46,7 +62,9 @@ public class event_menu extends AppCompatActivity implements View.OnClickListene
             case R.id.sumo_btn:
                 goToSumo();
                 break;
-
+            case R.id.settings_btn:
+                //TODO: make activity to settings and display basic user information.
+                break;
             default:
                 break;
         }
