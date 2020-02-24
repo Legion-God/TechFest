27ff5534ca01807c6f74f9ff22100f1ae272125e
Shubhendra Rajkarne <srajkarne9@gmail.com>
2020-02-24 23:12:53 +0530

    team delete done

--- app/src/main/java/com/techno/googlyyy/coord_sumo/coord_registered_team.java
+++ app/src/main/java/com/techno/googlyyy/coord_sumo/coord_registered_team.java
@@ -4,11 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DownloadManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -39,14 +46,27 @@ public class coord_registered_team extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference("sumo/teams");
         listViewTeams = findViewById(R.id.listView_teams);
 
+        listViewTeams.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                cmn_team_structure single_sumo_team = teams.get(position);
+                //deleteDialog(all_sumo_teams.getTeam_num()+"", all_sumo_teams.getTeam_name());
+
+                deleteDialog(single_sumo_team.getTeam_name(),single_sumo_team.getTeam_num()+"");
+
+                return true;
+            }
+        });
+
         teams = new ArrayList<>();//stores teams
     }
 
+    //refreshes activity 
     private void refresh(){
         finish();
-        overridePendingTransition(0,0);
+        //overridePendingTransition(0,0);
         startActivity(getIntent());
-        overridePendingTransition(0,0);
+        //overridePendingTransition(0,0);
     }
 
     @Override
@@ -87,5 +107,37 @@ public class coord_registered_team extends AppCompatActivity {
         Query query = mDatabase.orderByChild("team_num");
         query.addListenerForSingleValueEvent(queryValueListener);
     }
+
+    private void deleteTeam(String tnum){
+        DatabaseReference del = FirebaseDatabase.getInstance().getReference("sumo/teams").child("team"+tnum);
+
+        del.removeValue();
+        Toast.makeText(this, "Team Deleted", Toast.LENGTH_SHORT).show();
+
+        refresh();
+    }
+
+    private void deleteDialog(String tname,final String tnum){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete?");
+        builder.setMessage("Team Number: "+tnum+"\n"+"Team Name: "+tname)
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteTeam(tnum);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
 
 }
